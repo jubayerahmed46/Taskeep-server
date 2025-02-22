8cb73cfa10185bc76c4a4b1d1514dfae6b182b68
jubayer <jubayerahmed46@gmail.com>
2025-02-22 08:18:01 +0600

    upd: changed file structure and make it simple

--- server.js
+++ server.js
@@ -1,19 +1,10 @@
 const express = require("express");
 const cors = require("cors");
-const { connectDB } = require("./src/db/db");
 const rootHtml = require("./rootHtml");
 require("dotenv").config();
-
-// import routes and pass
-const tasksRoute = require("./src/routes/tasks");
-const usersRoute = require("./src/routes/users");
-
-// setUp port and application
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 const port = process.env.PORT || 55555;
 const app = express();
-
-// call the the function before calling 'app' object. otherwise, there might error.
-connectDB();
 
 // middlewares
 app.use(
@@ -21,10 +12,91 @@ app.use(
 );
 app.use(express.json());
 
-// routes
-app.use("/api/tasks", tasksRoute);
-app.use("/api/users", usersRoute);
+const uri = process.env.MONGOD_CONNECT_URI;
+// const uri = process.env.LOCAL_URI;
+console.log(process.env.MONGOD_CONNECT_URI);
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+(async function () {
+  try {
+    await client.connect();
+    const db = client.db(process.env.DB_NAME);
+    const tasksCollection = db.collection("tasks");
+    const usersCollection = db.collection("users");
+
+    // create a new user and store it in the db
+    app.post("/api/users", async (req, res) => {
+      const { email, displayName, userId } = req.body;
+
+      const isExistUser = await usersCollection.findOne({ email });
+      if (isExistUser) {
+        return res.send("User Exist!");
+      }
+
+      const result = await usersCollection.insertOne({
+        userId,
+        email,
+        displayName,
+      });
+
+      res.send(result);
+    });
+
+    app.post("/api/tasks", async (req, res) => {
+      const doc = req.body;
+      doc.timestamp = Date.now();
+
+      const result = await tasksCollection.insertOne(doc);
+
+      if (result.acknowledged) {
+        return res
+          .status(201)
+          .send({ message: "Task created", _id: result.insertedId });
+      }
+    });
+
+    app.get("/api/tasks/:email", async (req, res) => {
+      const email = req.params.email;
+
+      const result = await tasksCollection.find({ email }).toArray();
+
+      res.send(result);
+    });
+
+    app.patch("/api/tasks/:id", async (req, res) => {
+      const { status } = req.body;
+      const { id } = req.params;
+
+      await tasksCollection.updateOne(
+        { _id: new ObjectId(id) },
+        { $set: { status } }
+      );
+
+      const updatedTask = { id, status };
+      res.send({ message: "Task status updated", updatedTask });
+    });
+
+    app.delete("/api/tasks/:id", async (req, res) => {
+      const { id } = req.params;
+
+      await tasksCollection.deleteOne({ _id: new ObjectId(id) });
+      res.send({ message: "Task status updated", id });
+    });
+
+    // catch server error here
+  } catch (error) {
+    console.error(`Error: ${error.message}`);
+  }
+})();
+
+// root page with plain html
 app.get("/", (_, res) => {
   res.send(rootHtml);
 });
--- src/collections/collections.js
+++
@@ -1,6 +0,0 @@
-const { getDB } = require("../db/db");
-
-const tasksCollection = () => getDB().collection("tasks");
-const usersCollection = () => getDB().collection("users");
-
-module.exports = { tasksCollection, usersCollection };
